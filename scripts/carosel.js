444e5e1edbe7b1730ee5159cd9a62a861fb566e4
youssieframadan <youssief.r@gmail.com>
2021-07-29 22:16:21 +0200

    first_commit

---
+++ scripts/carosel.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.carousel').slick({
+        slidesToShow: 3,
+        dots:true,
+        centerMode: true,
+        autoplay:true,
+        autoplaySpeed: 1000,
+
+        });
+  });
