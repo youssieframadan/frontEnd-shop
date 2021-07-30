93bc95ce909851ad919da86b7671a3fc3983f9b0
youssieframadan <youssief.r@gmail.com>
2021-07-30 19:07:05 +0200

    added product page

--- scripts/carosel.js
+++ scripts/carosel.js
@@ -5,6 +5,20 @@ $(function(){
         centerMode: true,
         autoplay:true,
         autoplaySpeed: 1000,
-
-        });
+    });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        // fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
   });
