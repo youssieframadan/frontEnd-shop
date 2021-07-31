87f615e65284511830bc20e3c3e52a927512192f
youssieframadan <youssief.r@gmail.com>
2021-07-31 20:28:35 +0200

    fixed slider error

--- scripts/slider.js
+++ scripts/slider.js
@@ -9,6 +9,7 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
+  if(slides.length === 0) return;
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
